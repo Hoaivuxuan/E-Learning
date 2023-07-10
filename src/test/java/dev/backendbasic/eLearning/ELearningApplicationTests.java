b85c65935f85e4f2ce23b61371ce233ac45a49de
Hoai <vuxuanhoai28@gmail.com>
2023-07-10 16:20:57 +0700

    add

---
+++ src/test/java/dev/backendbasic/eLearning/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.backendbasic.eLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/ptit/Main.java
@@ -0,0 +1,9 @@
+package ptit;
+
+public class Main {
+    public static void main(String[] args) {
+        String txt = "Hello World";
+        System.out.println(txt.toLowerCase());
+        System.out.println(txt.toUpperCase());
+    }
+}
